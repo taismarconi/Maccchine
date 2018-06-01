c1bcfa4efe51e3d1da798e8b869de99ed479dbb6
taismarconi <39117396+taismarconi@users.noreply.github.com>
2018-06-01 16:23:01 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,71 @@
 /**
-  *<img src="/home/taismarconi/Scrivania/Programmazione 2/prog2/media">
-  *@mainpage BENVENUTO
-  *@brief
-  *MACCCHINE è un gioco sviluppato con Allegro 5.<br>
-  *L'idea di base è un semplice gioco di intrattenimento dai comandi semplici ed immediati.
-  *Di seguito troverete il codice commentato e le varie spiegazioni per il suo funzionamento.
-  *Buona lettura!
-  *@author Mahmoud Marzak & Taisia Marconi
+  * @mainpage Benvenuto nella documentazione di Maccchine \n
+  * Maccchine è un semplice endless ispirato a giochi dello stesso
+  * genere. \n
+  * La musica e le immagini sono tutte proveniente dal web da fonti
+  * free-to-use e quindi non soggetti a copyright.\n
   *
-  *<h4>Link alle pagine dei file:</h4>
-  * • main.cpp<br>
-  * • init.cpp<br>
-  * • road.cpp<br>
-  * • prova.cpp<br>
-  * • mov.cpp<br>
-  * • nemici.cpp<br>
-  * • data.h<br>
   *
-  *Portabilità: non su diverse architetture<br>
-  *Tipo eseguibile: Dinamico<br>
-  *Numero file: 7<br>
-  *Debugging: -Wall & Tracing<br>
-  *Pagina principale: main.cpp<br>
+  * <b>Istruzioni di gioco:</b>\n
+  * Il gioco è esclusivamente giocabile in singolo giocatore.
+  * \nLo scopo è arrivare il più lontano possibile evitando le macchine
+  * provenienti frontalmente.\n
+  * La macchina è controllabile con le quattro frecce direzionali ed 
+  * è possibile mettere in pausa il gioco. 
+  * \nIl gameover può essere causato da:
+  * <ul>
+    <li>Andare contro ad una o più macchine "nemiche"</li>
+  * </ul>
+  * 
+  * 
+  * \n<b>Divisione in moduli:</b>\n
+  * <ul>
+    <li><b>function.h</b>: modulo dedicato alle funzioni della gestione di gioco.</li>
+    <li><b>data.h</b>: modulo dedicato alle strutture e alle variabili costanti.</li>
+  * </ul>
+  * 
   *
-  *<h2>Comandi</h2>
-  *Il movimento del giocatore dipende dalle 4 frecce direzionali.<br>
-  *Il comando SPAZIO permette nei vari stadi del gioco di iniziare, mettere in pausa e ricominciare.<br>
-  *L'uscita è effettuata tramite l'ESC oppure usando la semplice X della finestra di gioco.
+  * <b>Per iniziare la navigazione, andare su <li>main.cpp</li>.</b>
+  * 
+  * <h4>Link alle pagine dei file:</h4>
+  * • <li>main.cpp</li><br>
+  * • <li>start_finish.cpp</li><br>
+  * • <li>game_core.cpp</li><br>
+  * • <li>move_road.cpp</li><br>
+  * • <li>move_road.cpp</li><br>
+  * • <li>enemies.cpp</li><br>
+  * • <li>data.h</li><br>
+  * • <li>function.h</li><br>
   *
-  *<h2>Istruzioni</h2>
-  *Il gioco presenta una schermata iniziale con una sintetica spiegazione del funzionamento.<br>
-  *Allo start il giocatore dovrà evitare di collidere con le auto "nemiche", che si generano
-  *casualmente su due file principali, e su 4 corsie.<br>
-  *Più il giocatore sarà bravo, più la velocità aumenterà e il livello di difficoltà si alzerà.<br>
-  *In caso di collisione il giocatore potrà scegliere se uscire o ricominciare.<br>
-  *Il punteggio è calcolato automaticamente con l'avanzamento del gioco.<br>
   *
-  *<h2> Come compilarlo</h2>
-  *Per la creazione dell'eseguibile è stato creato l'apposito Makefile nella directory principale.<br>
-  *In caso di problemi, in alternativa, entrare nella Directory contenente i file del gioco
-  *e compilarlo mediante il comando sottostante:<br>
-  *g++ -Wall  -o cars main.cpp init.cpp road.cpp prova.cpp mov.cpp nemici.cpp `pkg-config --cflags --libs allegro-5.0 allegro_acodec-5.0  
-  *allegro_audio-5.0 allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0 allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 	
-  *allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`
+  * <b>N.B. PROBLEMI DI COMPILAZIONE</b>
+  * Per la creazione dell'eseguibile è stato creato l'apposito makefile nella
+  * directory principale.
+  * In caso di problemi, in alternativa, entrare nella Directory contenente i file del
+  * gioco e compilarlo mediante il comando sottostante:\n
+  * g++ -Wall -o maccchine main.cpp start_finish.cpp move_road.cpp 
+  * move_car.cpp game_core.cpp enemies.cpp `pkg-config --cflags --libs
+  * allegro-5.0 allegro_acodec-5.0 allegro_audio-5.0
+  * allegro_color-5.0 allegro_dialog-5.0 allegro_font-5.0
+  * allegro_image-5.0 allegro_main-5.0 allegro_memfile-5.0 
+  * allegro_physfs-5.0 allegro_primitives-5.0 allegro_ttf-5.0`\n\n
+  *
+  * Nel caso si utilizzasse Ubuntu e desse problemi di compilazione,
+  * utilizzare la stessa riga per compilare ma togliendo il '.0'
+  * nella versione di allegro.
   */
+
+  /**
+   * @file
+   *
+   * File da cui inizia l'esecuzione del programma.<br>
+   * <b>Funzioni</b>
+   * - ::main
+   *
+   * @author Taisia Marconi e Mahmoud Marzak
+   *
+   */
+  
   
 #include <stdio.h>
 #include <cstdlib> 
@@ -59,46 +81,35 @@
 
 using namespace std;
 
-/**Funzione principale.
- * Il richiamo alla funzione "gestione_menu" permette la creazione 
+/**
+ * Funzione principale. <br>
+ * Il richiamo alla funzione ::gestione_menu permette la creazione 
  * degli oggetti principali e l'inizio del gioco.
+ * 
  */
  
 int main(int argc, char** argv) {
 
-	/** 
-	 * Inizializzazione interfaccia grafica.
-	 */
     	if(!al_init()){
         	al_show_native_message_box(NULL, NULL, NULL, "Could not inizialize Allegro5", NULL, ALLEGRO_MESSAGEBOX_ERROR);
         	return -1;
     	}
 
-	/** 
-	 * Creazione del display.
-	 */
-    	ALLEGRO_DISPLAY *display = al_create_display(SCREEN_W, SCREEN_H); /*<Oggetto ALLEGRO_DISPLAY, rappresentante il "display". */
+    	ALLEGRO_DISPLAY *display = al_create_display(SCREEN_W, SCREEN_H); /**Oggetto ALLEGRO_DISPLAY, rappresentante il "display". */
     	al_set_window_title(display, "Cars");
     	
-    	/**
-    	 * Controllo della creazione del display: se riscontra problemi il programma viene chiuso.
-    	 */
     	if(!display){
         	al_show_native_message_box(display, "Sample Title", "Display Settings", "Display not create", NULL,ALLEGRO_MESSAGEBOX_ERROR);
         	return -1;   
     	}
     
-       	al_init_primitives_addon(); /*<Inizializzazione addon principali. */
-    	al_install_keyboard(); /*<Inizializzazione input per l'interazione col gioco. */
-    	al_init_image_addon(); /*<Inizializzazione addon immagini. */
+       	al_init_primitives_addon(); /**Inizializzazione addon principali. */
+    	al_install_keyboard(); /**Inizializzazione input per l'interazione col gioco. */
+    	al_init_image_addon(); /**Inizializzazione addon immagini. */
    
-	gestione_menu(display); /*<Apertura della pagina principale del gioco. */
+	gestione_menu(display); /**Apertura della pagina principale del gioco. */
 
-	/**
-	 * Distruzione del display e uscita dal gioco.
-	 */
     	al_destroy_display(display);
     	
     	return 0;
 }
-
--- start_finish.cpp
+++ start_finish.cpp
@@ -1,3 +1,25 @@
+/**
+  * @file
+  *
+  * File con le funzioni degli stati del gioco.<br>
+  * <b>Funzioni</b>
+  * - ::replay
+  * - ::gestione_menu
+  * - ::break_game
+  * - ::game_over
+  *
+  *
+  * @param ::num_enemies
+  * @param ::speedinc
+  * @param ::road_y
+  * @param ::doexit
+  * @param ::redraw
+  * @param ::key
+  * 
+  * @author Taisia Marconi e Mahmoud Marzak
+  *
+  */
+    
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib> 
@@ -18,266 +40,336 @@
 
 using namespace std;
 
+/**Variabile intera che determina il numero di auto da evitare. */
 int num_enemies = 6;
-float speedinc = 0.2;
-float road_y = 2.0;
+/**Variabile di tipo float che gestisce l'incremento della velocità strada. */
+float speedinc = 0.2; 
+/**Varibile di tipo float che gestisce il movimento della strada. */
+float road_y = 2.0; 
 
-bool doexit = false;
+/**Booleana per l'uscita dei cicli while.*/
+bool doexit = false; 
+/**Booleana per l'aggiornamento dello schermo.*/
 bool redraw = true;
 
+/**Chiavi per gestire gli eventi del gioco.*/
 enum MYKEYS {
    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE, KEY_ESCAPE
 };
 
-bool key[6] = { false, false, false, false, false, false};
+/**Chiavi del gioco settate a false in attesa di essere attivate.*/
+bool key[6] = {false, false, false, false, false, false};
 
+/**Funzione per reimpostare gli stati iniziali del gioco.*/
 void replay(car_t &c, enemy_t *&ene, score_t &score) {
-	
-	c.x = SCREEN_W / 2.0 + 22.0;
-	c.y = SCREEN_H - CAR_H - 75.0;
-	
-	ene[0].x = 195; ene[0].y = - CAR_H; ene[0].imm = al_load_bitmap("media/bcar.png");
-	ene[1].x = 325; ene[1].y = - CAR_H; ene[1].imm = al_load_bitmap("media/gcar.png");
-	ene[2].x = 455; ene[2].y = - CAR_H; ene[2].imm = al_load_bitmap("media/wwcar.png");
-	
-	ene[3].x = 530; ene[3].y = - CAR_H - SCREEN_H/2; ene[3].imm = al_load_bitmap("media/bcar.png");
-	ene[4].x = 395; ene[4].y = - CAR_H - SCREEN_H/2; ene[4].imm = al_load_bitmap("media/gcar.png");
-	ene[5].x = 260; ene[5].y = - CAR_H - SCREEN_H/2; ene[5].imm = al_load_bitmap("media/wwcar.png");
-	
-	score.punteggio = 0;
-	
-	speedinc = 0.2;
-	num_enemies = 6;
-	road_y = 2.0;
-	
-	for (int i = 0; i < 6; i++) {
-		key[i] = false;
-	}
+  
+  /**Posizionamento orizzontale del giocatore*/
+  c.x = SCREEN_W / 2.0 + 22.0;
+  /**Posizionamento verticale del giocatore*/
+  c.y = SCREEN_H - CAR_H - 75.0; 
+  
+  /**Inizializzazione primo nemico*/
+  ene[0].x = 195; ene[0].y = - CAR_H; ene[0].imm = al_load_bitmap("media/bcar.png");
+  /**Inizializzazione secondo nemico*/
+  ene[1].x = 325; ene[1].y = - CAR_H; ene[1].imm = al_load_bitmap("media/gcar.png");
+  /**Inizializzazione terzo nemico*/
+  ene[2].x = 455; ene[2].y = - CAR_H; ene[2].imm = al_load_bitmap("media/wwcar.png");
+  /**Inizializzazione quarto nemico*/
+  ene[3].x = 530; ene[3].y = - CAR_H - SCREEN_H/2; ene[3].imm = al_load_bitmap("media/bcar.png");
+  /**Inizializzazione quinto nemico*/ 
+  ene[4].x = 395; ene[4].y = - CAR_H - SCREEN_H/2; ene[4].imm = al_load_bitmap("media/gcar.png");
+  /**Inizializzazione sesto nemico*/ 
+  ene[5].x = 260; ene[5].y = - CAR_H - SCREEN_H/2; ene[5].imm = al_load_bitmap("media/wwcar.png");
+  
+  /**Azzeramento del punteggio*/
+  score.punteggio = 0;
+  
+  /**Reimpostazione dell'incremento della velocità*/
+  speedinc = 0.2;
+  /**Reimpostazione del numero di nemici*/ 
+  num_enemies = 6;
+  /**Riposizionamento del background*/ 
+  road_y = 2.0;
+  
+  /**Ciclo per settare tutte le chiavi a false*/
+  for (int i = 0; i < 6; i++) {
+    key[i] = false;
+  }
 }
 
+/**Funzione per l'apertura della schermata iniziale.*/
 void gestione_menu(ALLEGRO_DISPLAY* display){
-    	
-   	ALLEGRO_TIMER *timer = NULL;
-	timer = al_create_timer(1.0 / FPS);
-   	if(!timer) {
-      		fprintf(stderr, "failed to create timer!\n");
-   	}
-   	
-	al_install_audio();
-   	al_init_acodec_addon();
-    	al_init_font_addon();
-    	al_init_ttf_addon();
-   	
-   	al_reserve_samples(10);
-   	
-   	ALLEGRO_BITMAP *road = NULL;
-    	road = al_load_bitmap("media/road.png");
-	if(!road) {
-   		fprintf(stderr, "failed to create gestione_menu bitmap\n");
-		al_destroy_display(display);
+
+	/**Oggetto ALLEGRO_TIMER, utilizzato per la gestione del timer, ovvero la frequenza con la quale si ripetono le azioni del gioco.*/
+    	ALLEGRO_TIMER *timer = NULL;
+    	/**Inizializzazione del timer*/
+  	timer = al_create_timer(1.0 / FPS);
+    	if(!timer) {
+          	fprintf(stderr, "failed to create timer!\n");
     	}
-	
-    	al_draw_bitmap(road, 0, 0, 0);
-    	
-    	ALLEGRO_FONT *titlefont = al_load_font("media/arcade.ttf", 70, 0);
-    	ALLEGRO_FONT *startfont = al_load_font("media/arcade.ttf", 40, 0);
-    	ALLEGRO_FONT *instructionsfont = al_load_font("media/arcade.ttf", 25, 0);
-    	al_draw_text(titlefont, al_map_rgb(0, 0, 0), 260, 100, 0, "Maccchine");
-    	al_draw_text(startfont, al_map_rgb(0, 0, 0), 155, 250, 0, "Premi spazio per cominciare");
-    	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 525, 585, 0, "Esc per uscire");
-    	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 150, 585, 0, "Usa  le  frecce  per");
-    	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 150, 600, 0, "evitare  le   auto");
-	
-	car_t c;
+    
+  	al_install_audio();
+    	al_init_acodec_addon();
+      	al_init_font_addon();
+      	al_init_ttf_addon();
+    
+    	al_reserve_samples(10);
+    
+    	/**Oggetto ALLEGRO_BITMAP contenente l'immagine del background. */
+    	ALLEGRO_BITMAP *road = NULL;
+    	/**Inizializzazione del background*/
+      	road = al_load_bitmap("media/road.png");
+  	if(!road) {
+      		fprintf(stderr, "failed to create gestione_menu bitmap\n");
+    		al_destroy_display(display);
+      	}
+  
+      	al_draw_bitmap(road, 0, 0, 0);
+      
+      	/**Oggetto ALLEGRO_FONT, rappresentante il font utilizzato per stampare il titolo. */
+      	ALLEGRO_FONT *titlefont = al_load_font("media/arcade.ttf", 70, 0);
+      	/**Oggetto ALLEGRO_FONT, rappresentante il font utilizzato per stampare il comando di start. */
+      	ALLEGRO_FONT *startfont = al_load_font("media/arcade.ttf", 40, 0);
+      	/**Oggetto ALLEGRO_FONT, rappresentante il font utilizzato per stampare le istruzioni principali. */
+      	ALLEGRO_FONT *instructionsfont = al_load_font("media/arcade.ttf", 25, 0);
+      
+      	al_draw_text(titlefont, al_map_rgb(0, 0, 0), 260, 100, 0, "Maccchine");
+      	al_draw_text(startfont, al_map_rgb(0, 0, 0), 155, 250, 0, "Premi spazio per cominciare");
+      	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 525, 585, 0, "Esc per uscire");
+      	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 150, 585, 0, "Usa  le  frecce  per");
+      	al_draw_text(instructionsfont, al_map_rgb(0, 0, 0), 150, 600, 0, "evitare  le   auto");
+  
+  	/**Oggetto di tipo car_t(struttura) rappresentante il giocatore principale. */
+  	car_t c;
 
-	c.x = SCREEN_W / 2.0 + 22.0;
-	c.y = SCREEN_H - CAR_H - 75.0;
-	
-    	ALLEGRO_BITMAP *car = NULL;
-    	car = al_load_bitmap("media/car.png");
-    	
-    	ALLEGRO_BITMAP *car_dx = NULL;
-    	car_dx = al_load_bitmap("media/carR.png");
-    	
-    	ALLEGRO_BITMAP *car_sx = NULL;
-    	car_sx = al_load_bitmap("media/carL.png");
-    	
-    	if(!car || !car_dx || !car_sx) {
-    	    fprintf(stderr, "failed to create car bitmap\n");
-    	    al_destroy_font(titlefont);
-    	    al_destroy_font(startfont);
-    	    al_destroy_font(instructionsfont);
-    	    al_destroy_bitmap(road);
-    	    al_destroy_display(display);
+	/**Posizionamento orizzontale del giocatore.*/
+  	c.x = SCREEN_W / 2.0 + 22.0;
+  	/**Posizionamento verticale del giocatore.*/
+  	c.y = SCREEN_H - CAR_H - 75.0;
+  
+  	/** Oggetto ALLEGRO_BITMAP contenente l'immagine del giocatore. */
+      	ALLEGRO_BITMAP *car = NULL;
+      	car = al_load_bitmap("media/car.png");
+      
+      	/**Oggetto ALLEGRO_BITMAP contenente l'immagine del giocatore in movimento a dx. */
+      	ALLEGRO_BITMAP *car_dx = NULL; 
+      	car_dx = al_load_bitmap("media/carR.png");
+      
+      	/**Oggetto ALLEGRO_BITMAP contenente l'immagine del giocatore in movimento a sx. */
+      	ALLEGRO_BITMAP *car_sx = NULL;
+      	car_sx = al_load_bitmap("media/carL.png");
+      
+      	if(!car || !car_dx || !car_sx) {
+        	fprintf(stderr, "failed to create car bitmap\n");
+        	al_destroy_font(titlefont);
+          	al_destroy_font(startfont);
+          	al_destroy_font(instructionsfont);
+          	al_destroy_bitmap(road);
+          	al_destroy_display(display);
         }
-	
-	c.imm[0] = car;
-	c.imm[1] = car_dx;
-	c.imm[2] = car_sx;
-	
-    	al_draw_bitmap(car, c.x, c.y, 0);
-	
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    	event_queue = al_create_event_queue();
-   	if(!event_queue) {
-      		fprintf(stderr, "failed to create event_queue!\n");
-      		
-      		for (int i = 0; i < 3; i++) {
-      			al_destroy_bitmap(c.imm[i]);
-      		}
-      		
-      		al_destroy_bitmap(road);
-      		al_destroy_display(display);
-      		al_destroy_timer(timer);
-   	}
+  
+  	c.imm[0] = car;
+  	c.imm[1] = car_dx;
+  	c.imm[2] = car_sx;
+  
+      	al_draw_bitmap(car, c.x, c.y, 0);
+  
+  	/**Oggetto ALLEGRO_EVENT_QUEUE, rappresentante gli eventi. */
+  	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+      	event_queue = al_create_event_queue();
+    	if(!event_queue) {
+        	fprintf(stderr, "failed to create event_queue!\n");
+          
+          	for (int i = 0; i < 3; i++) {
+            		al_destroy_bitmap(c.imm[i]);
+          	}
+          
+          	al_destroy_bitmap(road);
+          	al_destroy_display(display);
+          	al_destroy_timer(timer);
+    	}
  
-   	al_register_event_source(event_queue, al_get_display_event_source(display));
-   	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-   	al_register_event_source(event_queue, al_get_keyboard_event_source()); 
+    	al_register_event_source(event_queue, al_get_display_event_source(display));
+    	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    	al_register_event_source(event_queue, al_get_keyboard_event_source()); 
    
-   	al_flip_display();
+    	al_flip_display();
  
-   	al_start_timer(timer);
+    	al_start_timer(timer);
  
-   	while(!doexit) {
-      		ALLEGRO_EVENT ev;
-      		al_wait_for_event(event_queue, &ev);
-      		
-		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-         		break;
-      		}
-      		
-      		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-         		switch(ev.keyboard.keycode) {
-         			case ALLEGRO_KEY_ESCAPE:
-				       	doexit = false;
-				       	break;
-				case ALLEGRO_KEY_SPACE:
-				       	key[KEY_SPACE] = true;
-				       	break;
-			 }
-		}       
+ 	/**Ciclo per la gestione degli eventi*/
+    	while(!doexit) {
+    		/**Oggetto ALLEGRO_EVENT che identifica il tipo di evento*/
+          	ALLEGRO_EVENT ev;
+          	al_wait_for_event(event_queue, &ev);
+          	
+          	/**Chiusura del gioco tramite pulsante x*/
+    		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            		break;
+          	}
+          	
+          	/**Gestione eventi da tastiera*/
+          	else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            		switch(ev.keyboard.keycode) {
+            			/**Inizio del gioco*/
+              			case ALLEGRO_KEY_ESCAPE:
+                		doexit = false;
+                		break;
+        			
+        			/**Inizializzazione uscita dal gioco*/
+        			case ALLEGRO_KEY_SPACE:
+                		key[KEY_SPACE] = true;
+                		break;
+       			}
+    		}       
 
-      		else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
-			 switch(ev.keyboard.keycode) {
-			    	case ALLEGRO_KEY_ESCAPE:
-			    		doexit = true;
-			       		break;
-
-			    	case ALLEGRO_KEY_SPACE:
-         				move_game(display, timer, event_queue, road, c);
-			        	key[KEY_SPACE] = false;
-			        	break;
-			 }
-      		}
-   	}
+          	else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+       			switch(ev.keyboard.keycode) {
+       				/**Uscita dal gioco*/
+            			case ALLEGRO_KEY_ESCAPE:
+              			doexit = true;
+                		break;
+				
+				/**Apertura frame del gioco*/
+            			case ALLEGRO_KEY_SPACE:
+                		move_game(display, timer, event_queue, road, c);
+                		key[KEY_SPACE] = false;
+                		break;
+       			}
+          	}
+    	}
 }
 
+/**
+ * Funzione per mettere in pausa il gioco.
+ */
 void break_game(ALLEGRO_EVENT_QUEUE* event_queue, bool &pausa) {
 
-	ALLEGRO_FONT *pausafont;
-    	ALLEGRO_FONT *nextfont;
+	/**Oggetto di tipo ALLEGRO_FONT	che segnala lo stato di pausa*/
+  	ALLEGRO_FONT *pausafont;
+  	/**Oggetto di tipo ALLEGRO_FONT che descrive come uscire dallo stato di pausa*/	
+     	ALLEGRO_FONT *nextfont;
 
-    	pausafont = al_load_font("media/arcade.ttf", 90, 0);
-    	nextfont = al_load_font("media/arcade.ttf", 30, 0);
+      	pausafont = al_load_font("media/arcade.ttf", 90, 0);
+      	nextfont = al_load_font("media/arcade.ttf", 30, 0);
 
-	al_draw_text(pausafont, al_map_rgb(250, 0, 0), 245, 100, 0, "PAUSA");
-	al_draw_text(nextfont, al_map_rgb(250, 0, 0), 220, 200, 0, "Premi SPAZIO per riprendere");
-	
-	al_flip_display();
+  	al_draw_text(pausafont, al_map_rgb(250, 0, 0), 245, 100, 0, "PAUSA");
+  	al_draw_text(nextfont, al_map_rgb(250, 0, 0), 220, 200, 0, "Premi SPAZIO per riprendere");
+  
+  	al_flip_display();
 
-	while(!doexit) {
-      		ALLEGRO_EVENT ev;
-      		al_wait_for_event(event_queue, &ev);
-      		       
+	/**Ciclo per la gestione degli eventi*/
+  	while(!doexit) {
+  		/**Oggetto ALLEGRO_EVENT che identifica il tipo di evento*/
+          	ALLEGRO_EVENT ev;
+          	al_wait_for_event(event_queue, &ev);
+                
+                /**Chiusura del gioco tramite pulsante x*/
                 if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-         		doexit = true;
-         		pausa = false;
-         		break;
-      		}
-      		
-      		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-         		switch(ev.keyboard.keycode) {
-         			case ALLEGRO_KEY_ESCAPE: {
-				       	doexit = false;
-				} break;
-				
-				case ALLEGRO_KEY_SPACE: {
-					key[KEY_SPACE] = true;
-				} break;
-			}
-		}	   
+            		doexit = true;
+            		pausa = false;
+            		break;
+          	}
+          
+          	/**Gestione eventi da tastiera*/
+          	else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            		switch(ev.keyboard.keycode) {
+            			/**Inizializzazione uscita dal gioco*/
+              			case ALLEGRO_KEY_ESCAPE: {
+                			doexit = false;
+        			} break;
+        			/**Inizializzazione ripresa del gioco*/
+        			case ALLEGRO_KEY_SPACE: {
+          				key[KEY_SPACE] = true;
+        			} break;
+      			
+      			}
+    		}    
 
-      		else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
-			 switch(ev.keyboard.keycode) {
-			 	case ALLEGRO_KEY_ESCAPE: {
-			 		pausa = false;
-			    		doexit = true;
-			    	} break;
-			       		
-			 	case ALLEGRO_KEY_SPACE: {
-			        	al_destroy_font(pausafont);
-                                	al_destroy_font(nextfont);
-                                	pausa = false;
-			       	} return;
-			 }
-		}
-      	}	
-	
+          	else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+       			switch(ev.keyboard.keycode) {
+       				/**Uscita dal gioco*/
+        			case ALLEGRO_KEY_ESCAPE: {
+		  			pausa = false;
+		      			doexit = true;
+            			} break;
+                		/**Uscita stato di pausa*/
+        			case ALLEGRO_KEY_SPACE: {
+                			al_destroy_font(pausafont);
+                                  	al_destroy_font(nextfont);
+                                  	pausa = false;
+              			} return;
+       			
+       			}
+    		}
+        }
 }
 
+/**
+ * Funzione per il gameover.
+ */
 void game_over(ALLEGRO_EVENT_QUEUE *event_queue, car_t &c, enemy_t *&ene, bool &collisione, ALLEGRO_BITMAP *boom) {
 
-	cout<<"Collisione"<<endl;
-	
-	ALLEGRO_FONT *gameoverfont;
-    	ALLEGRO_FONT *nextgamefont;
+  	//cout<<"Collisione"<<endl;
+  	
+  	/**Oggetto di tipo ALLEGRO_FONT	che segnala lo stato di gameover*/
+  	ALLEGRO_FONT *gameoverfont;
+  	/**Oggetto di tipo ALLEGRO_FONT	che indica le istruzioni successive al gameover*/
+      	ALLEGRO_FONT *nextgamefont;
 
-    	gameoverfont = al_load_font("media/arcade.ttf", 90, 0);
-    	nextgamefont = al_load_font("media/arcade.ttf", 30, 0);
+      	gameoverfont = al_load_font("media/arcade.ttf", 90, 0);
+      	nextgamefont = al_load_font("media/arcade.ttf", 30, 0);
 
-	al_draw_text(gameoverfont, al_map_rgb(250, 0, 0), 225, 100, 0, "GAME OVER");
-	al_draw_text(nextgamefont, al_map_rgb(250, 0, 0), 220, 200, 0, "Premi SPAZIO per ricominciare");
-    	al_draw_text(nextgamefont, al_map_rgb(250, 0, 0), 265, 235, 0, "Premi ESC per uscire");
-    	
-	al_flip_display();
+  	al_draw_text(gameoverfont, al_map_rgb(250, 0, 0), 225, 100, 0, "GAME OVER");
+  	al_draw_text(nextgamefont, al_map_rgb(250, 0, 0), 220, 200, 0, "Premi SPAZIO per ricominciare");
+      	al_draw_text(nextgamefont, al_map_rgb(250, 0, 0), 265, 235, 0, "Premi ESC per uscire");
+      
+  	al_flip_display();
 
-	while(!doexit) {
-      		ALLEGRO_EVENT ev;
-      		al_wait_for_event(event_queue, &ev);
-      		       
+	/**Ciclo per la gestione degli eventi*/
+  	while(!doexit) {
+  		/**Oggetto ALLEGRO_EVENT che identifica il tipo di evento*/
+          	ALLEGRO_EVENT ev;
+          	al_wait_for_event(event_queue, &ev);
+                 
+                /**Chiusura del gioco tramite pulsante x*/
                 if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-         		doexit = true;
-         		collisione = false;
-         		break;
-      		}
-      		
-      		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-         		switch(ev.keyboard.keycode) {
-         			case ALLEGRO_KEY_ESCAPE: {
-				       	doexit = false;
-				} break;
-				
-				case ALLEGRO_KEY_SPACE: {
-					key[KEY_SPACE] = true;
-				} break;
-			}
-		}	   
+            		doexit = true;
+            		collisione = false;
+            		break;
+          	}
+          	
+          	/**Gestione eventi da tastiera*/
+          	else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+            		switch(ev.keyboard.keycode) {
+            			/**Inizializzazione uscita dal gioco*/
+              			case ALLEGRO_KEY_ESCAPE: {
+                			doexit = false;
+        			} break;
+        			
+        			/**Inizializzazione restart del gioco*/
+        			case ALLEGRO_KEY_SPACE: {
+          				key[KEY_SPACE] = true;
+        			} break;
+      			}
+    		}    
 
-      		else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
-			 switch(ev.keyboard.keycode) {
-			 	case ALLEGRO_KEY_ESCAPE: {
-			 		collisione = false;
-			    		doexit = true;
-			    	} break;
-			       		
-			 	case ALLEGRO_KEY_SPACE: {
-			 		al_draw_bitmap(boom, -200, -200, 0);
-			        	al_destroy_font(gameoverfont);
-                                	al_destroy_font(nextgamefont);
-                                	collisione = false;
-			       	} return;
-			 }
-		}
-      	}	
+          	else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+       			switch(ev.keyboard.keycode) {
+       				/**Uscita dal gioco*/
+        			case ALLEGRO_KEY_ESCAPE: {
+          				collisione = false;
+              				doexit = true;
+            			} break;
+                
+                		/**Restart del gioco*/
+        			case ALLEGRO_KEY_SPACE: {
+          				al_draw_bitmap(boom, -200, -200, 0);
+                			al_destroy_font(gameoverfont);
+                                  	al_destroy_font(nextgamefont);
+                                  	collisione = false;
+              			} return;
+       			}
+    		}
+        } 
 }
